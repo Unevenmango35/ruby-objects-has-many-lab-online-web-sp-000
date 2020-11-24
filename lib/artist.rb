a72304ebacb8af8d8aa3d3d437efe0fade1f1787
Rosie Choi <rosiechoimanso@gmail.com>
2020-11-24 03:32:58 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -15,15 +15,23 @@ class Artist
     song.artist = self
   end
 
-  def self.song_count
-    @@song_count
-  end
-  
 
 
 def songs
   Song.all.select {|song| song.artist == self}
 end
+
+def add_song_by_name(name)
+    song = Song.new(name)
+    self.songs << song
+    song.artist = self
+    @@song_count += 1
+  end
+
+  def self.song_count
+    @@song_count
+  end
+
 
 
 end
