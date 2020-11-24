f3b9b0dfb71f25870a5b9099108aae8f4c966b76
Rosie Choi <rosiechoimanso@gmail.com>
2020-11-24 03:32:55 +0000

    Automatically backed up by Learn

---
+++ lib/post.rb
@@ -0,0 +1,25 @@
+class Post
+
+attr_accessor :name, :artist
+
+@@all =[]
+
+def initialize(name)
+  @name = name
+  @@all << self
+end
+
+def self.all
+  @@all
+end
+
+def artist_name
+    if self.artist
+      self.artist.name
+    else
+      nil
+    end
+  end
+
+
+end
